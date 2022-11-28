d4cf48e988d1fc1c2c866a6a7d2560761842a1df
thiagooruby <trs3@aluno.ifal.edu.br>
2022-11-28 17:31:13 -0300

    add a web page

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
-## Encrypto API
+# Encrypto API
 
-A flask API to generate keys, encrypting and decrypting using RSA method
+A flask API to generate keys, encrypting and decrypting using RSA method.
+
+#### Group members:
+
+- Thiago Ribeiro
+- Donvicton Monteiro
+- José Endson dos Santos
+- Edeilson da Costa
+
+## Generate Keys
+
+To generate RSA keys, just send a POST request to https://encrypto-api-com.onrender.com/generate_key with a json like this:
+
+```json
+{
+    "p" : "13",
+    "q" : "17",
+    "e" : "5"
+}
+```
+
+## Encrypting
+
+To encrypting a message, just send a POST request to https://encrypto-api-com.onrender.com/encrypting with a json like this:
+
+```json
+{
+    "message" : "Ola mundo",
+    "n" : "221",
+    "e" : "5"
+}
+```
+## Decrypting
+
+To decrypting a crypted message, just send a POST request to https://encrypto-api-com.onrender.com/decrypting with a json like this:
+
+```json
+{
+    "crypted" : "152 13 32 214 131 133 19 31 152",
+    "p" : "13",
+    "q" : "17",
+    "e" : "5"
+}
+```
+
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, request, jsonify, flash, make_response
 from rsa_crypto import RSA
+import re
 
 def create_app():
 
@@ -8,11 +9,60 @@ def create_app():
     # Instancing a rsa
     rsa = RSA()
 
+    @app.route('/generate-page', methods=['GET', 'POST'])
+    def generate_page():
+        keys = ''
+        if request.method == 'POST':
+            p = request.form['p']
+            q = request.form['q']
+            e = request.form['ege']
+            #print(values)
+            try:
+                keys = RSA().generatePublicKey(int(p), int(q), int(e))
+            except:
+                keys = ("Certify that P and Q are prime numbers", "Check if E is coprime with (p - 1) x (q - 1)")
+            print(len(keys))
+        return render_template('generate.html', keys = keys)
+
+    @app.route('/encrypting-page', methods=['GET', 'POST'])
+    def encrypting_page():
+        crypted = ''
+        if request.method == 'POST':
+            message = request.form['message']
+            message = re.sub('[^a-zA-z ]', '', message)
+            print(message)
+            n = request.form['n']
+            e = request.form['eenc']
+            #print(values)
+            try:
+                crypted = RSA().encrypting(message, int(n), int(e))
+            except:
+                crypted = "There are something wrong with your keys"
+            print(crypted)
+        return render_template('encrypting.html', message = crypted)
+    
+    @app.route('/decrypting-page', methods=['GET', 'POST'])
+    def decrypting_page():
+        decrypted = ''
+        if request.method == 'POST':
+            crypted = request.form['crypted']
+            crypted = re.sub('[^0-9 ]', '', crypted)
+            p = request.form['p']
+            q = request.form['q']
+            e = request.form['edec']
+            #print(values)
+            try:
+                decrypted = RSA().decrypting(crypted, int(p), int(q), int(e))
+            except:
+                crypted = "There are something wrong with your keys or crypted message"
+            print(decrypted)
+        return render_template('decrypting.html', message = decrypted)
+
     @app.route('/generate_key', methods=['POST'])
     def generate_key():
         values = request.json
-        n, e = RSA().generatePublicKey(values['p'], values['q'], values['e'])
-        resp = {"n" : n, "e" : e}
+        n, e = RSA().generatePublicKey(int(values['p']), int(values['q']), int(values['e']))
+        resp = {"n" : str(n), "e" : str(e)}
         return make_response(
             jsonify(resp)
         )
@@ -20,7 +70,7 @@ def create_app():
     @app.route("/encrypting", methods=['POST'])
     def encrypting_message():
         values = request.json
-        crypted = RSA().encrypting(values['message'], values['n'], values['e'])
+        crypted = RSA().encrypting(values['message'], int(values['n']), int(values['e']))
         resp = {"crypted" : crypted}
         return make_response(
             jsonify(resp)
@@ -29,7 +79,7 @@ def create_app():
     @app.route("/decrypting", methods=['POST'])
     def decrypting_message():
         values = request.json
-        message = RSA().decrypting(values['crypted'], values['p'], values['q'], values['e'])
+        message = RSA().decrypting(values['crypted'], int(values['p']), int(values['q']), int(values['e']))
         resp = {"message" : message}
         return make_response(
             jsonify(resp)
